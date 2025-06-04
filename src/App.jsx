a34f3ec1f7c81372e43b67912340c4b74e6b7307
DELL <sangyeop0529@gmail.com>
2025-06-04 14:55:26 +0900

    app 설정 및 header 생성 feat tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+import SearchHeader from "./components/SearchHeader";
+
 function App() {
-  return <>setting</>;
+  return (
+    <>
+      <SearchHeader />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
