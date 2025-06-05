97b713aed2b6f5d71a40bed538a7e9a3e1b6304f
DELL <sangyeop0529@gmail.com>
2025-06-05 12:35:15 +0900

    context 사용

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
 import App from "./App.jsx";
+import NotFound from "./pages/NotFound.jsx";
 import Videos from "./pages/Videos.jsx";
 import VideoDetail from "./pages/VideoDetail.jsx";
-import NotFound from "./pages/NotFound.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -4,20 +4,18 @@ import { useParams } from "react-router-dom";
 import VideoCard from "../components/VideoCard";
 import FakeYoutube from "../api/fakeYoutube";
 import Youtube from "../api/youtube";
+import { useYoutubeApi } from "../context/youtubeApiContext";
 
 const Videos = () => {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos, // data를 videos로 변경해서 사용
   } = useQuery({
     queryKey: ["videos", keyword], // /videos/{keyword}.json
-    queryFn: () => {
-      // const youtube = new FakeYoutube();
-      const youtube = new Youtube();
-      return youtube.search(keyword); // FakeYoutube 클래스를 사용하여 동영상 검색
-    },
+    queryFn: () => youtube.search(keyword),
   });
 
   return (
