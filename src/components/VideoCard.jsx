33b45c686f7f7f6415010fcb7b8b7717d69655d7
DELL <sangyeop0529@gmail.com>
2025-06-05 13:34:40 +0900

    channelInfo 와 RelatedVideos 파일 생성

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { getRelativeTime } from "../util/date";
+import { useNavigate } from "react-router-dom";
 
 const VideoCard = ({ video }) => {
   const { channelTitle, title, thumbnails, publishedAt } = video.snippet;
+  const nav = useNavigate();
   return (
-    <li>
+    <li onClick={() => nav(`videos/watch/${video.id}`, { state: { video } })}>
       <img className="w-full" src={thumbnails.medium.url} alt={title} />
       <div>
         <p className="font-semibold my-2 line-clamp-2">{title}</p>
---
+++ src/pages/ChannelInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChannelInfo = ({ id, name }) => {
+  return <div>{name}</div>;
+};
+
+export default ChannelInfo;
